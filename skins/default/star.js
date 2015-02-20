4191e960e91132104f0a61e3cdcd58a0e2e99779
1Sam Online <csh@korea.com>
2015-02-21 03:49:25 +0900

    Update star.js

--- skins/default/star.js
+++ skins/default/star.js
@@ -2,83 +2,78 @@ jQuery(document).ready(function(){
 	// 위젯 최상위 부모 div에 적용된 overflow: hidden 속성 제거
 	// [ 별 1개 스킨 ] ribonicon 등 과의 조화를 위해 display: inline-block 추가
 	jQuery( ".star_rate2" ).parent( "div" ).parent( "div" ).css({overflow: "", display: "inline-block"});
-	jQuery( ".star_rate2" ).parent( "div" ).css({padding: ""})
+	jQuery( ".star_rate2" ).parent( "div" ).css({padding: ""});
 	// [ 별 10개 스킨 ]
 	jQuery( "#outline_default" ).parent( "div" ).parent( "div" ).css({overflow: ""});
 	
 	// 툴팁
 	//jQuery("div.rated_users span").tooltip();
-	jQuery('[data-toggle="tooltip"]').tooltip();	
+	jQuery('[data-toggle="tooltip"]').tooltip();
 
 	// 셀렉터를 rating i, a, img 태그로 하였으며 추가 가능함
 	jQuery('.rating i').hover(function(){
+		var uid = this.id;
+		var uids = uid.split("_");
 
-		var point = jQuery(this).attr("point");
-		var document_srl = this.id;
+		jQuery('#rateit_'+uids[1]).css({visibility: "hidden"});
+		for(var x=1;x<=uids[0];x++){jQuery('#'+x+'_'+uids[1]).css('color', '#E9D801');}
 
-		jQuery('#rateit_'+document_srl).css({visibility: "hidden"});
-
-		for(var i=1;i<=point;i++){
-			// 2개의 셀렉터를 충족해야함(id, point)
-			jQuery("[id="+this.id+"][point="+i+"]").css('color', '#E9D801');
-		}
-
-		switch(point){
+		switch(uids[0]){
 			case '1':
-			jQuery('#rateit_'+document_srl).css({visibility: "visible", color: "red"});
-			jQuery('#rateit_'+document_srl).html('&nbsp;< 1 > 내 시간...');
+			jQuery('#rateit_'+uids[1]).css({visibility: "visible", color: "red"});
+			jQuery('#rateit_'+uids[1]).html('&nbsp;< 1 > 내 시간...');
 			break;
 			case '2':
-			jQuery('#rateit_'+document_srl).css({visibility: "visible", color: "red"});
-			jQuery('#rateit_'+document_srl).html('&nbsp;< 2 > 난감');
+			jQuery('#rateit_'+uids[1]).css({visibility: "visible", color: "red"});
+			jQuery('#rateit_'+uids[1]).html('&nbsp;< 2 > 이게 뭐야...');
 			break;
 			case '3':
-			jQuery('#rateit_'+document_srl).css({visibility: "visible", color: "#b8860b"});
-			jQuery('#rateit_'+document_srl).html('&nbsp;< 3 > 일쌤의 의도는?');
+			jQuery('#rateit_'+uids[1]).css({visibility: "visible", color: "#b8860b"});
+			jQuery('#rateit_'+uids[1]).html('&nbsp;< 3 > 감독의 의도는?');
 			break;
 			case '4':
-			jQuery('#rateit_'+document_srl).css({visibility: "visible", color: "#b8860b",});
-			jQuery('#rateit_'+document_srl).html('&nbsp;< 4 > 1회용...');
+			jQuery('#rateit_'+uids[1]).css({visibility: "visible", color: "#b8860b",});
+			jQuery('#rateit_'+uids[1]).html('&nbsp;< 4 > 1회용...');
 			break;
 			case '5':
-			jQuery('#rateit_'+document_srl).css({visibility: "visible", color: "green"});
-			jQuery('#rateit_'+document_srl).html('&nbsp;< 5 > 그냥 그렇군');
+			jQuery('#rateit_'+uids[1]).css({visibility: "visible", color: "green"});
+			jQuery('#rateit_'+uids[1]).html('&nbsp;< 5 > 그냥 그렇군');
 			break;
 			case '6':
-			jQuery('#rateit_'+document_srl).css({visibility: "visible", color: "green"});
-			jQuery('#rateit_'+document_srl).html('&nbsp;< 6 > 그럭저럭');
+			jQuery('#rateit_'+uids[1]).css({visibility: "visible", color: "green"});
+			jQuery('#rateit_'+uids[1]).html('&nbsp;< 6 > 그럭저럭');
 			break;
 			case '7':
-			jQuery('#rateit_'+document_srl).css({visibility: "visible", color: "blue"});
-			jQuery('#rateit_'+document_srl).html('&nbsp;< 7 > 볼만하네');
+			jQuery('#rateit_'+uids[1]).css({visibility: "visible", color: "blue"});
+			jQuery('#rateit_'+uids[1]).html('&nbsp;< 7 > 볼만하네');
 			break;
 			case '8':
-			jQuery('#rateit_'+document_srl).css({visibility: "visible", color: "orange"});
-			jQuery('#rateit_'+document_srl).html('&nbsp;< 8 > 일쌤 최고');
+			jQuery('#rateit_'+uids[1]).css({visibility: "visible", color: "orange"});
+			jQuery('#rateit_'+uids[1]).html('&nbsp;< 8 > 괜찮네');
 			break;
 			case '9':
-			jQuery('#rateit_'+document_srl).css({visibility: "visible", color: "#f88"});
-			jQuery('#rateit_'+document_srl).html('&nbsp;< 9 > 역시 일쌤');
+			jQuery('#rateit_'+uids[1]).css({visibility: "visible", color: "#f88"});
+			jQuery('#rateit_'+uids[1]).html('&nbsp;< 9 > 잘 만들었군');
 			break;
 			case '10':
-			jQuery('#rateit_'+document_srl).css({visibility: "visible", color: "#9B70F0"});
-			jQuery('#rateit_'+document_srl).html('&nbsp;< 10 > 일쌤 짱짱맨');
+			jQuery('#rateit_'+uids[1]).css({visibility: "visible", color: "#9B70F0"});
+			jQuery('#rateit_'+uids[1]).html('&nbsp;< 10 > 또 봐야지!');
 		}
-	})
+	});
 
 	jQuery('.rating i').mouseout(function() {
-		jQuery('#rateit_'+this.id).css({visibility: "hidden"});
-		jQuery("[id="+this.id+"]").css('color', 'gray');
-	})
+		var uid = this.id;
+		var uids = uid.split("_");
+		jQuery('#rateit_'+uids[1]).css({visibility: "hidden"});
+		for(var x=1;x<=uids[0];x++){jQuery('#'+x+'_'+uids[1]).css('color', 'gray');}
+	});
+
 });
 
 // 별점 부과
 function callback_add_default(result){
 	//alert(result['error']+'/'+result['message']+'/'+result['html_code']);
-	var document_srl = result['document_srl'];
-	var member_srl = result['member_srl'];
-
-	var user = 'rate_' + member_srl;
+	var user = 'rate_' + result['member_srl'];
 	//user2 변수를 제대로 인식되도록 수정 해야함
 	var user2 = 'onclick=\"removestar(\"' + user + '\") ';
 	user2 ='';
@@ -91,32 +86,32 @@ function callback_add_default(result){
 
 	if(result['message'] == "success"){
 
-		var star_html_code = get_star_html_default(result['star_max'],result['converted_average'],document_srl);
+		var star_html_code = get_star_html_default(result['star_max'],result['converted_average'],result['document_srl']);
 		//alert(star_html_code);
 		
-		jQuery("#final_"+document_srl).html(star_html_code);
-		jQuery(adder).appendTo("#rated_users_"+document_srl); // 평점 준사람 출력
-		jQuery("#ment_"+document_srl).html('<i class="fa fa-user"></i>:');
-		jQuery("#s_average_"+document_srl).html(result['average']); //부가 점수
-		jQuery("#s_persons_"+document_srl).html(result['persons']);
+		jQuery("#final_"+result['document_srl']).html(star_html_code);
+		jQuery(adder).appendTo("#rated_users_"+result['document_srl']); // 평점 준사람 출력
+		jQuery("#ment_"+result['document_srl']).html('<i class="fa fa-user"></i>:');
+		jQuery("#s_average_"+result['document_srl']).html(result['average']); //부가 점수
+		jQuery("#s_persons_"+result['document_srl']).html(result['persons']);
 		jQuery('#res').css({opacity: 0.0, visibility: "visible"});
 
 		var already = ("감사합니다.");
 
-		jQuery("#second_s_"+document_srl).fadeOut(99);
-		jQuery("#already_"+document_srl).css({visibility: "visible", "font-size":"12px",color: "red","padding-left":"15px"});
-		jQuery("#already_"+document_srl).fadeIn(100);
-		jQuery("#already_"+document_srl).html(already);
-		jQuery("#already_"+document_srl).fadeOut(8000);
+		jQuery("#second_s_"+result['document_srl']).fadeOut(99);
+		jQuery("#already_"+result['document_srl']).css({visibility: "visible", "font-size":"12px",color: "red","padding-left":"15px"});
+		jQuery("#already_"+result['document_srl']).fadeIn(100);
+		jQuery("#already_"+result['document_srl']).html(already);
+		jQuery("#already_"+result['document_srl']).fadeOut(8000);
 		
 	}else if(result['message'] == 'already'){
 		var already = ("이미 별점을 주셨습니다.");
 		
 		jQuery('#res').css({opacity: 0.0, visibility: "visible"});
-		jQuery("#already_"+document_srl).css({visibility: "visible"});
-		jQuery("#already_"+document_srl).fadeIn(100);
-		jQuery("#already_"+document_srl).html(already);
-		jQuery("#already_"+document_srl).fadeOut(5000);
+		jQuery("#already_"+result['document_srl']).css({visibility: "visible"});
+		jQuery("#already_"+result['document_srl']).fadeIn(100);
+		jQuery("#already_"+result['document_srl']).html(already);
+		jQuery("#already_"+result['document_srl']).fadeOut(5000);
 	}else {
 		alert("오류");
 	}
@@ -125,42 +120,40 @@ function callback_add_default(result){
 // 별점 삭제
 function callback_remove_default(result){
 	//alert(result['error']+'/'+result['message']+'/'+result['html_code']);
-	var document_srl = result['document_srl'];
-	var member_srl = result['member_srl'];
 
-	//var user_div = 'rate_'+ member_srl+'_'+document_srl;//반영될 div의 아디값
+	var user_div = 'rate_'+ result['member_srl']+'_'+result['document_srl'];//반영될 div의 아디값
 	//var star_max = document.getElementById('star_max').value;
 	//var full_point = document.getElementById('full_point').value
 	//var widget_skin = document.getElementById('widget_skin').value
 
 	if(result['message'] == 'success'){
 		//별코드
-		var star_html_code = get_star_html_default(result['star_max'], result['converted_average'],document_srl);
+		var star_html_code = get_star_html_default(result['star_max'], result['converted_average'],result['document_srl']);
 
-		jQuery("#final_"+document_srl).html(star_html_code);
+		jQuery("#final_"+result['document_srl']).html(star_html_code);
 
-		jQuery("#ment_"+document_srl).html('Average rating:');
-		jQuery("#s_average_"+document_srl).html(result['average']); //부가정보
-		jQuery("#s_persons_"+document_srl).html(result['persons']);
+		jQuery("#ment_"+result['document_srl']).html('<svg class="star-icon-10" viewBox="0 0 40 40"><use xlink:href="#icon-star-checked-full" filter="url(#drop-shadow)"></use></svg> :');
+		jQuery("#ment_"+result['document_srl']).html('Average rating:');
+		jQuery("#s_average_"+result['document_srl']).html(result['average']); //부가정보
+		jQuery("#s_persons_"+result['document_srl']).html(result['persons']);
 
 		var already = ("별점을 삭제 했습니다.");
 
-		// 유저 아이콘 제거, 3가지 셀렉터 체크(class, id, member_srl)
-		jQuery("[class~='rated_user'][id="+document_srl+"][member_srl="+member_srl+"]").css({display: "none"});
-		//jQuery("#"+user_div).css({display: "none"});
-		jQuery("#second_s_"+document_srl).fadeOut(99);
-		jQuery("#already_"+document_srl).css({visibility: "visible", "font-size":"12px",color: "red","padding-left":"15px"});
-		jQuery("#already_"+document_srl).fadeIn(100);
-		jQuery("#already_"+document_srl).html(already);
-		jQuery("#already_"+document_srl).fadeOut(4000);
+		//jQuery("#"+user_div).html('');
+		jQuery("#"+user_div).css({display: "none"});
+		jQuery("#second_s_"+result['document_srl']).fadeOut(99);
+		jQuery("#already_"+result['document_srl']).css({visibility: "visible", "font-size":"12px",color: "red","padding-left":"15px"});
+		jQuery("#already_"+result['document_srl']).fadeIn(100);
+		jQuery("#already_"+result['document_srl']).html(already);
+		jQuery("#already_"+result['document_srl']).fadeOut(8000);
 
 	}else if(result['message'] == 'already'){
 		var already = ("이미 별점을 주셨습니다.");
 
-		jQuery("#already_"+document_srl).css({visibility: "visible"});
-		jQuery("#already_"+document_srl).fadeIn(100);
-		jQuery("#already_"+document_srl).html(already);
-		jQuery("#already_"+document_srl).fadeOut(3000);
+		jQuery("#already_"+result['document_srl']).css({visibility: "visible"});
+		jQuery("#already_"+result['document_srl']).fadeIn(100);
+		jQuery("#already_"+result['document_srl']).html(already);
+		jQuery("#already_"+result['document_srl']).fadeOut(5000);
 	}
 }
 
